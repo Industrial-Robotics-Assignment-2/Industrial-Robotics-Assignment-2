dd49587f719d2b53febdd55c54224f260f460da0
Industrial-Robotics-Assignment-2 <liam.dascalos@student.uts.edu.au>
2025-10-22 04:15:46 +1100

    Arduino eStop Sketch
    Arduino code for emergency stop

---
+++ LA2 Final/eStop/eStop.ino
@@ -0,0 +1,24 @@
+int buttonPin = 5;
+int irPin = 4;
+
+int buttonState;
+int irState;
+
+void setup() {
+  Serial.begin(9600);  // Start serial communication at 9600 baud
+  pinMode(buttonPin, INPUT);
+  pinMode(irPin, INPUT);
+}
+
+void loop() {
+  buttonState = digitalRead(buttonPin);
+  irState = digitalRead(irPin);
+
+  if (buttonState == HIGH) {
+    Serial.println("ESTOP");
+  }
+
+  if (irState == HIGH) {
+    Serial.println("LIGHT CURTAIN");
+  }
+}
